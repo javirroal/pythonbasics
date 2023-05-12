0fcb70b1712feb7d4c712233795dbb729b20f483
Rodrigo Arenas <jarenasl@pucp.edu.pe>
2023-05-11 22:43:36 -0500

    Inicial

---
+++ tipos de datos/datos-simples.py
@@ -0,0 +1,18 @@
+"string"
+'string'
+
+'''tus datos son:
+    nombre:  javier
+    apellido arenas'''
+    
+"""tus datos son:
+    nombre:  javier
+    apellido arenas'"""
+    
+
+40
+
+40.4
+
+False
+True
